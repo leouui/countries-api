d70233e2b5819fc6c132744241ee8522661d6d74
Leonardo Moreno <leoui.moreno@gmail.com>
2022-01-10 15:05:06 -0500

    styles added to DashRoutes container

--- src/routes/routes/DashRoutes.jsx
+++ src/routes/routes/DashRoutes.jsx
@@ -3,7 +3,7 @@ import { Home ,CountryDetails} from "../../views"
 import { NavBar } from "../../layout"
 
 const DashRoutes = () => {
-    return <div>
+    return <div className="content__principal">
         <NavBar/>
         <Routes>
             <Route path="/" element={<Home/>} />
