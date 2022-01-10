466f260ee8ef952b164eb467d3c6568482e7999b
Leonardo Moreno <leoui.moreno@gmail.com>
2022-01-10 14:44:51 -0500

    NavBar(togle-theme,responsive)

--- src/layout/NavBar/index.jsx
+++ src/layout/NavBar/index.jsx
@@ -1,14 +1,24 @@
+import { useEffect, useState } from "react"
 import "./styles.css"
 
-const NavBar = () => {
-    return <nav className="navbar w-100">
-        <div className="navbar__container w-page d-flex fx-between max-w-100">
-            <div>
-                Where in the world?
-            </div>
+// false === "ligth"
+// true === "dark"
 
-            <div>
-                Dark Mode
+const NavBar = () => {
+    const [theme,setTheme] = useState(JSON.parse(localStorage.getItem("dark-theme")))
+
+    useEffect(()=>{
+        if(theme) document.body.setAttribute("data-theme","dark") 
+        else document.body.setAttribute("data-theme","ligth")
+        localStorage.setItem("dark-theme",theme)
+    },[theme])
+
+    return <nav className="navbar w-100">
+        <div className="navbar__container w-page h-100 d-flex fx-between fy-center">
+            <h1 className="navbar__title">Where in the world?</h1>
+
+            <div className="navbar__darkmode-togle" onClick={() => setTheme(theme => !theme)}>
+                <i className={`navbar__icon-moon ${!theme ?"far fa-moon":"fas fa-moon"}`}></i>Dark Mode 
             </div>
         </div>
     </nav>
